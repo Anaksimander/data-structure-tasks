307b7caff1bb1d6c71f0a962da4e2ea91ae11527
Anaksimander <a.likhanov@g.nsu.ru>
2022-02-02 14:51:54 +0700

    решена задача "телефонная книга"

---
+++ data structure tasks/Hash tables/PhoneBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data_structure_tasks.Hash_tables
+{
+    //https://stepik.org/lesson/41562/step/1?unit=20016
+    class PhoneBook
+    {
+        public string[] names;
+        public PhoneBook()
+        {
+            names = new string[10000000];
+        }
+        public void Add(int number, string name)
+        {
+            names[number] = name;
+        }
+        public void Del(int number)
+        {
+            if(names[number] != null)
+            {
+                names[number] = null;
+            }
+        }
+        public string Find(int number)
+        {
+            if(names[number] == null)
+            {
+                return "not found";
+            }
+            else
+            {
+                return names[number];
+            }
+        }
+    }
+}
--- data structure tasks/Program.cs
+++ data structure tasks/Program.cs
@@ -5,28 +5,41 @@ using System.Linq;
 using data_structure_tasks.Basic_data_structures;
 using data_structure_tasks.Priority_Queues;
 using data_structure_tasks.Disjoint_set_systems;
+using data_structure_tasks.Hash_tables;
 
 namespace data_structure_tasks
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            
-            Queue<int> answer = new Queue<int>();
+        {           
+            int n  = int.Parse(Console.ReadLine());
+            Queue<string> answer = new Queue<string>();
+            string[] str;
+            PhoneBook phoneBook = new PhoneBook();
+            int temp;
 
-            int[] temp = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
-            int tableNum = temp[0];
-            int actionNum = temp[1];
-            int[] tableSizes = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
-
-            MergingTables set = new MergingTables(tableSizes);
-
-            for (int i = 0; i < actionNum; i++)
+            for (int i = 0; i < n; i++)
             {
-                temp = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
-                set.Union(temp[0], temp[1]);
-                answer.Enqueue(set.Max);
+                str = Console.ReadLine().Split(' ');
+
+                switch (str[0])
+                {
+                    case "add":
+                        temp = int.Parse(str[1]);
+                        phoneBook.Add(temp, str[2]);
+                        break;
+                    case "del":
+                        temp = int.Parse(str[1]);
+                        phoneBook.Del(temp);
+                        break;
+                    case "find":
+                        temp = int.Parse(str[1]);
+                        answer.Enqueue(phoneBook.Find(temp));
+                        break;
+                    default:
+                        break;
+                }
             }
 
             foreach (var item in answer)
@@ -34,6 +47,7 @@ namespace data_structure_tasks
                 Console.WriteLine(item);
             }
 
+            
             Console.Read();
         }
     }

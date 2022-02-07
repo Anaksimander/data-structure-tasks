fc573a92381fc79d6ccaf3f2c8d8cd50b88348f5
Anaksimander <a.likhanov@g.nsu.ru>
2022-02-07 18:16:40 +0700

    Решил задачи

---
+++ data structure tasks/Hash tables/ChainHashing.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data_structure_tasks.Hash_tables
+{
+    class ChainHashing
+    {
+        LinkedList<string>[] mass;
+        double x = 263;
+        double p = 1000000007;
+        double m;
+
+        Queue<string> answer = new Queue<string>();
+
+        public ChainHashing(int size)
+        {
+            mass = new LinkedList<string>[size];
+            m = size;
+            for (int i = 0; i < m; i++)
+            {
+                mass[i] = new LinkedList<string>();
+            }                    
+        }
+
+        private int H(string str)
+        {
+            double h = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                h = (h + (str[i] * Pow(x, i))) % p;     
+            }
+            //h = h % p;
+            h = h % m;
+            return (int)h;
+        }
+
+        private double Pow(double x, int y)
+        {
+            double result = x;
+            if (y == 0)
+                return 1.0;
+            else if (y == 1)
+            {
+                return x;
+            }
+            else
+                for (int i = 2; i <= y; i++)
+                {
+                    result *= x;
+                    result = result % p;
+                }
+            return result;
+        }
+
+        public void Add(string str)
+        {
+            int h = H(str);
+            if (mass[h].Find(str) == null)
+                mass[h].AddFirst(str);
+        }
+        public void Del(string str)
+        {
+            int h = H(str);
+            if(mass[h].Find(str) != null)
+                mass[h]?.Remove(mass[h]?.Find(str));
+        }
+        public void Find(string str)
+        {
+            int h = H(str);
+            if (mass[h].Find(str) == null)
+                answer.Enqueue("no");
+            else
+                answer.Enqueue("yes");
+        }
+        public void Check(int i)
+        {
+            StringBuilder sb = new StringBuilder();
+            //string temp;
+            if (mass[i].Count == 0)
+                answer.Enqueue(" ");
+            else
+                foreach (var item in mass[i])
+                {
+                    sb.Append($"{item} ");
+                }
+                answer.Enqueue(sb.ToString());
+        }
+        public void printAnswer()
+        {
+            foreach (var item in answer)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- data structure tasks/Program.cs
+++ data structure tasks/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 using data_structure_tasks.Basic_data_structures;
 using data_structure_tasks.Priority_Queues;
@@ -12,43 +14,23 @@ namespace data_structure_tasks
     class Program
     {
         static void Main(string[] args)
-        {           
-            int n  = int.Parse(Console.ReadLine());
-            Queue<string> answer = new Queue<string>();
-            string[] str;
-            PhoneBook phoneBook = new PhoneBook();
-            int temp;
+        {
+            Parallel.For(1, 10, Factorial);
 
-            for (int i = 0; i < n; i++)
+            Console.ReadLine();
+        }
+
+        static void Factorial(int x)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= x; i++)
             {
-                str = Console.ReadLine().Split(' ');
-
-                switch (str[0])
-                {
-                    case "add":
-                        temp = int.Parse(str[1]);
-                        phoneBook.Add(temp, str[2]);
-                        break;
-                    case "del":
-                        temp = int.Parse(str[1]);
-                        phoneBook.Del(temp);
-                        break;
-                    case "find":
-                        temp = int.Parse(str[1]);
-                        answer.Enqueue(phoneBook.Find(temp));
-                        break;
-                    default:
-                        break;
-                }
+                result *= i;
             }
-
-            foreach (var item in answer)
-            {
-                Console.WriteLine(item);
-            }
-
-            
-            Console.Read();
+            Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+            Console.WriteLine($"Факториал числа {x} равен {result}");
+            Thread.Sleep(3000);
         }
     }
 }
